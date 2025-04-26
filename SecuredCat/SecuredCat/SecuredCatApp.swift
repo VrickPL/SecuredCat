5d4854f53a6576c588f0cc4fbe7aecf60eb118aa
Jan Kazubski <jkazubski@icloud.com>
2025-04-26 13:03:17 +0200

    Initial projekt with Hello world text and app icon

---
+++ SecuredCat/SecuredCat/SecuredCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecuredCatApp.swift
+//  SecuredCat
+//
+//  Created by Jan Kazubski on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SecuredCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
